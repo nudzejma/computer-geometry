f155a48b95b178db225ef3552e0d4b213f933dda
nudzejma <nudzejma.imamovic@gmail.com>
2018-02-01 00:04:44 +0100

    changes to hurtado noy optimized algorithm

--- modules/hurtado_noy_optimized.py
+++ modules/hurtado_noy_optimized.py
@@ -14,7 +14,7 @@ from typing import List, Tuple, Deque
 import copy
 
 import itertools
-from anytree import Node, RenderTree, PreOrderIter
+from anytree import Node, RenderTree, PreOrderIter, LevelOrderIter
 from anytree.exporter import DotExporter
 
 from modules.primitives import graham_scan
@@ -22,26 +22,6 @@ from sorting_algorithms.benchmark import benchmark
 from structures.line_segment import Segment
 from structures.point import Point
 from structures.polygon import Polygon
-
-# def convert_to_list(s: str):
-#     return_list = []
-#     s = s[1:len(s) - 1]
-#     items_list = s.split('],');
-#
-#     for item in items_list:
-#         if item[0] == ' ':
-#             item = item[2:]
-#         else:
-#             item = item[1:]
-#
-#         if item[len(item) - 1] == ']':
-#             item = item[:-1]
-#
-#         new_tuple =item.split(', ')
-#
-#         return_list.append([int(new_tuple[0]), int(new_tuple[1])])
-#
-#     return return_list
 
 
 def triangulate_recursive(root_triangulation, current_n: int, end_n: int) -> None:
@@ -59,22 +39,25 @@ def triangulate_recursive(root_triangulation, current_n: int, end_n: int) -> Non
     '''
 
     pairs_list = root_triangulation.triangulation
+    list_of_transform_pairs = []
 
     for i, pair in enumerate(pairs_list):
 
         if pair[1] == current_n:
 
+
             list_of_child = list(pairs_list)
 
-            for j, previous_pair in enumerate(list_of_child[(i-1)::-1]):
-                if previous_pair[1] == current_n:
+            for transform_pair_index in list_of_transform_pairs:
 
-                    pp = previous_pair
-                    list_of_child.remove(previous_pair)
-                    list_of_child.insert(i-j-1,[pp[0], pp[1]+1])
+                pp = list_of_child[transform_pair_index]
+                list_of_child.remove(pp)
+                list_of_child.insert(transform_pair_index,[pp[0], pp[1]+1])
 
             list_of_child.insert(i+1, [pair[0], current_n + 1])
             list_of_child.append([current_n, current_n + 1])
+
+            list_of_transform_pairs.append(i)
 
             new_child = Node('Child_' + str(pair[0]) + '_' + str(pair[1]), parent=root_triangulation, triangulation=list_of_child)
             if ([current_n, current_n+1] == [end_n - 1, end_n]):
@@ -83,7 +66,7 @@ def triangulate_recursive(root_triangulation, current_n: int, end_n: int) -> Non
 
             triangulate_recursive(new_child, current_n+1, end_n)
 
-def triangulate(convex_polygon: Polygon):
+def triangulate(convex_polygon: Polygon) -> int:
     '''
 
     This function finds triangulations of convex polygon
@@ -93,33 +76,42 @@ def triangulate(convex_polygon: Polygon):
     Returns: number od triangulations
 
     '''
-    diagonal_list = []
-    diagonal_list.append([1, 2])
-    diagonal_list.append([1, 3])
-    diagonal_list.append([2, 3])
-    current_n = 3
-    root_triangulation = Node('root', triangulation=diagonal_list)
-    fm = time()
-    triangulate_recursive(root_triangulation, current_n, len(convex_polygon.points))
-    sm = time()
-    print('{}s'.format(sm - fm))
-    print(RenderTree(root_triangulation))
+    root_triangulation = Node('root', triangulation=[[1, 2], [1, 3], [2, 3]])
+    # fm = time()
+    triangulate_recursive(root_triangulation, 3, len(convex_polygon.points))
+    # sm = time()
+    # print('{}s'.format(sm - fm))
+    # print(RenderTree(root_triangulation))
 
     polygons_segments = []
-    for node1 in PreOrderIter(root_triangulation):
-        if node1.is_leaf:
-            node_triangulation = node1.triangulation
-            list_of_segments = []
-            for pair in node_triangulation:
+    for node_ in LevelOrderIter(root_triangulation, filter_=lambda n: n.triangulation[len(n.triangulation)-1][1] == len(convex_polygon.points)-1):
 
-                s = Segment(convex_polygon.points[pair[0]-1], convex_polygon.points[pair[1]-1])
-                list_of_segments.append(s)
+        polygons_segments.append(node_.triangulation)
 
-            polygons_segments.append(list_of_segments)
-    # for segments in polygons_segments[23]:
-    #     segments.draw(turtle, "red")
-    # turtle.done()
     return len(polygons_segments)
+
+def draw_triangulation(index_of_triangulation: int, polygon_segments: List, convex_polygon: Polygon) -> None:
+    '''
+    This function draws one triangulation of convex polygon
+    Args:
+        index_of_triangulation: which triangulation do we want to draw
+        polygon_segments: list of all trianngulations
+        convex_polygon: polygon
+
+    Returns: None
+
+    '''
+    list_of_segments = []
+    for pair in polygon_segments[index_of_triangulation]:
+
+        s = Segment(convex_polygon.points[pair[0] - 1], convex_polygon.points[pair[1] - 1])
+        list_of_segments.append(s)
+
+    for segments in list_of_segments:
+
+        segments.draw(turtle, "red")
+
+    turtle.done()
 
 def hurtado_noy_benchmark(input_list: List) -> None:
     '''
@@ -134,7 +126,7 @@ def hurtado_noy_benchmark(input_list: List) -> None:
     text_file = "hurtado_noy_benchmark_optimized.txt"
     f = open(text_file, 'a+')
     n = 4
-    while n < 17:
+    while n < 14:
         q = Polygon(input_list[:n])
         bench = benchmark(triangulate, q)
         f.write(str(len(q.points)) + ' ' + str(bench) + 's \n')
